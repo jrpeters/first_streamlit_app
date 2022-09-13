1fbf9a4916cbce20c550f024d9523eeb5f490368
Jeff Peters <jrpeters23@gmail.com>
2022-09-13 13:28:38 -0700

    Trying to add data to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Allow the user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('test')");
 
